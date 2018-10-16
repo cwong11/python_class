7d5101c9451e00c0e9c9aba28cbecf22fbe61f04
Cynthia Wong <cynthia.wong@wework.com>
2018-10-15 21:30:57 -0400

    First Commit

---
+++ validator.py
@@ -0,0 +1,42 @@
+"""
+Python Class
+Example - Space Validator
+"""
+
+print('Program Started')
+
+def calculate_efficiency(room_area, room_desk_count):
+
+    efficiency = room_area / room_desk_count
+    return efficiency
+
+
+
+
+area = 100
+desk_count = 4
+
+result = calculate_efficiency(100,4)
+result = calculate_efficiency(area, desk_count)
+
+
+
+
+space = {'name':empty,'area':empty,'width':'empty,'is_occupied":'empty'}
+
+
+
+
+width = 10
+height = 20>200
+
+result = calculate_area(10,20>200)
+result = calculate_area(width, height)
+
+make_upper_case
+
+validate_area
+
+
+print('Program Ended')
+
